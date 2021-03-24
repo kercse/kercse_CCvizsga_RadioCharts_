081bee140a4bcd9741ba31b5c827039e7fe2c5a4
kercse <69111507+kercse@users.noreply.github.com>
2021-03-24 17:25:02 +0100

    2 test out of 8

--- src/main/java/com/codecool/database/RadioCharts.java
+++ src/main/java/com/codecool/database/RadioCharts.java
@@ -1,6 +1,42 @@
 package com.codecool.database;
 
 
+import java.sql.*;
+import java.sql.DriverManager;
+
+
 public class RadioCharts {
-    //TODO: implement this class
+    private static String DB_URL;
+    private static String DB_USER;
+    private static String DB_PASSWORD;
+
+    public Connection connection = null;
+
+    public RadioCharts(String DB_URL, String DB_USER, String DB_PASSWORD) {
+        this.DB_URL = DB_URL;
+        this.DB_USER = DB_USER;
+        this.DB_PASSWORD = DB_PASSWORD;
+        getConnection();
+    }
+
+    public Connection getConnection() {
+        Connection connect = null;
+        try {
+            connect = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connect;
+    }
+
+    public String getMostPlayedSong() {
+
+        String sql = "";
+        return sql;
+    }
+
+    public String getMostActiveArtist() {
+        String sql = "";
+        return sql;
+    }
 }
